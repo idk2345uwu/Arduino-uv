e0e5fc38f4b9d516608808241bd32de5e173b877
idk2345uwu <163457466+idk2345uwu@users.noreply.github.com>
2024-12-20 09:03:55 -0300

    actualizacion

--- WriteSingleField/secrets.h
+++ WriteSingleField/secrets.h
@@ -6,3 +6,4 @@
 
 #define SECRET_CH_ID 0000000			// replace 0000000 with your channel number
 #define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
+#define SECRET_READ_APIKEY "XYZ"
